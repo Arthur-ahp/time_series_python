19f942036e51a43dccb7074bf7f5131409494398
Arthur Haupt Pacheco <arthurhauptpacheco@gmail.com>
2025-08-28 19:16:50 -0300

    Alface roxo

---
+++ alface_roxo.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Título: alface_roxo
+Descrição: Exercicío de python atualizando arquivo - alface roxo.
+Autor: Arthur Haupt Pacheco
+Data: 21/08/2025
+Versão: 1.0
+"""
+
+import sys
+import os
